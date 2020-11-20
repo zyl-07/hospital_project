d524c30313381cde86c60559c8ea7f8888d9d555
zyl-07 <3167824397@qq.com>
2020-11-20 13:42:31 +0800

    增加rbac权限控制表

---
+++ src/main/java/com/example/test/pojo/permission.java
@@ -0,0 +1,21 @@
+package com.example.test.pojo;
+
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.*;
+
+/**
+ * @author zyl
+ * 权限表
+ */
+@Entity
+@Table(name = "permssion")
+public class permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer pid;//权限id
+    String pname;//权限标识
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rid")
+    private role role;
+}
---
+++ src/main/java/com/example/test/pojo/role.java
@@ -0,0 +1,23 @@
+package com.example.test.pojo;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+public class role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer rid;
+    String rname;
+    @ManyToMany
+    @JoinTable(name = "user_role"
+        ,joinColumns  = @JoinColumn(name = "rid"),
+        inverseJoinColumns = @JoinColumn(name = "uid")
+    )
+    private List<user> users = new ArrayList<>();
+    @OneToMany(mappedBy = "role",cascade = CascadeType.ALL,orphanRemoval = true) //orphanRemoval 角色没有这个权限了，
+    // 就会把角色对应得权限表信息删除，CascadeType.ALL角色对权限有绝对控制权，角色删除了，权限表信息也会删除
+    private List<permission> permissions = new ArrayList<>();
+}
